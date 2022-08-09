48b3b24175599df3e6692914ee9d118d2d70d070
AlvinButt <arslanbutt300@gmail.com>
2022-08-09 20:14:35 +0500

    pods

---
+++ Pods/Target Support Files/Pods-tentwenty-tentwentyUITests/Pods-tentwenty-tentwentyUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tentwenty_tentwentyUITests : NSObject
+@end
+@implementation PodsDummy_Pods_tentwenty_tentwentyUITests
+@end
---
+++ Pods/Target Support Files/Pods-tentwentyTests/Pods-tentwentyTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_tentwentyTests : NSObject
+@end
+@implementation PodsDummy_Pods_tentwentyTests
+@end
